258695b40ba7dc2b49d21dd3a45dfbe3c57cd154
Brian Graham <brian.graham@experiencepoint.com>
2014-11-17 18:12:06 -0500

    Adds a serve script for local editing.

---
+++ serve.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+port=$1
+if [ $#  -ne  1 ]
+then
+  port=8000
+fi
+
+if [ $(uname -s) == "Darwin" ]
+then
+  open=open
+else
+  open=xdg-open
+fi
+
+$open http://localhost:$port/index.html && python -m http.server $port;
